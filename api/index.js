3019c766e3d0deded49154c3e6affd5410a83ad7
tinyjin <baram991103@gmail.com>
2018-10-25 15:46:17 +0900

    JWT 사용자 인증 구현, auth API 수정

--- api/index.js
+++ api/index.js
@@ -3,64 +3,22 @@ const users = require('./model/users'); // users 테이블 모델
 const app = express(); // express 생성
 const port = 8888; // 서버를 열 때 사용되는 포트 정보
 
+const cookieParser = require('cookie-parser');
+const Token = require('./token');
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(Token.checkToken);
 
-/**
- * hi 라우터
- */
-app.get('/hi', (req, res) => {
-    res.send('hi~');
-});
-
-
-/**
- * create 라우터 (데이터 생성)
- */
-app.post('/create', (req, res) => {
-    const user_id = req.body.user_id;
-    const user_pw = req.body.user_pw;
-
-    users.create({
-        user_id: user_id,
-        user_pw: user_pw,
-    }).then((user) => { // 콜백함수
-        res.json({
-            status: true,
-            resData: user,
-        });
-    }).catch((err) => { // 에러 핸들링 함수
-        res.json({
-            status: false,
-            resData: err,
-        });
-    });
-});
-
-
-/**
- * read 라우터 (데이터 조회)
- */
-app.get('/read/:user_id', (req, res) => {
-    const user_id = req.params.user_id;
-
-    users.findOne({
-        where: {
-            user_id
+app.get('/testAPI', (req, res) => {
+    res.json({
+        data: {
+            message: 'API 테스트 성공',
         }
-    }).then((user) => {
-        res.json({
-            status: true,
-            resData: user.dataValues,
-        })
-    }).catch((err) => {
-        res.json({
-            status: false,
-            resData: err,
-        })
     });
 });
 
-app.use('/register', require('./register'));
+app.use('/auth', require('./auth'));
 
 app.listen(port); // port로 서버 열기
 console.log('start server with 8888!');
