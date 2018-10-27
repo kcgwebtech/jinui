bace7f1df6f716e59c998070890657f83f7fdd3b
tinyjin <baram991103@gmail.com>
2018-10-27 18:32:07 +0900

    비밀번호 INPUT 타입추가

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express'); // 모듈을 불러오는 구문
-const users = require('./model/users'); // users 테이블 모델
 const app = express(); // express 생성
 const port = 8888; // 서버를 열 때 사용되는 포트 정보
 
